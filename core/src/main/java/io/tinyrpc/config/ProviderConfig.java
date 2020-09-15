95ea444c9bcbf01c2b33d3a7e571226443b10653
andaicheng <andaicheng@gmail.com>
2020-09-15 16:03:26 +0800

    tinyrpc

--- core/src/main/java/io/tinyrpc/config/ProviderConfig.java
+++ core/src/main/java/io/tinyrpc/config/ProviderConfig.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,4 +18,34 @@ public class ProviderConfig<T> extends AbstractInterfaceConfig implements Serial
      * 注册中心配置，可配置多个
      */
     protected List<RegistryConfig> registry;
+    /**
+     * 配置的协议列表
+     */
+    protected ServerConfig serverConfig;
+    /**
+     * 接口实现类引用
+     */
+    protected transient T ref;
+
+    public List<RegistryConfig> getRegistry() {
+        return registry;
+    }
+
+    public void setRegistry(List<RegistryConfig> registry) {
+        this.registry = registry;
+    }
+
+    /**
+     * 设置注册中心
+     *
+     * @param registry RegistryConfig
+     */
+    public void setRegistry(RegistryConfig registry) {
+        if (registry != null) {
+            if (this.registry == null) {
+                this.registry = new ArrayList<>();
+            }
+            this.registry.add(registry);
+        }
+    }
 }
