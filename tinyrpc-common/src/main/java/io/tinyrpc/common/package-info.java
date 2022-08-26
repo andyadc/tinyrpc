216ee17e53dec1692aa41096285bf20b2e4b6b96
andaicheng <andaicheng@gmail.com>
2022-08-26 17:01:46 +0800

    tiny rpc

--- tinyrpc-common/src/main/java/io/tinyrpc/common/package-info.java
+++ tinyrpc-common/src/main/java/io/tinyrpc/common/package-info.java
@@ -1,3 +1,4 @@
 package io.tinyrpc.common;
 
+// https://articles.zsxq.com/id_bal2cnmw3jbi.html
 // https://gitcode.net/l1028386804/bhrpc-learning/-/tree/bhrpc-chapter-04/bhrpc-common
--- tinyrpc-common/src/test/java/io/tinyrpc/common/test/service/HelloServiceImpl.java
+++ tinyrpc-common/src/test/java/io/tinyrpc/common/test/service/HelloServiceImpl.java
@@ -2,6 +2,11 @@ package io.tinyrpc.common.test.service;
 
 import io.tinyrpc.annotation.RpcService;
 
-@RpcService
+@RpcService(
+	interfaceClass = HelloService.class,
+	interfaceClassName = "io.tinyrpc.common.test.service.HelloService",
+	version = "2.0.0",
+	group = "tiny-g-1"
+)
 public class HelloServiceImpl {
 }
