23f3d1092bf91b28ad3c57a5d5135de67245d07a
andaicheng <andaicheng@gmail.com>
2022-10-17 17:21:23 +0800

    rpc

--- tinyrpc-provider/tinyrpc-provider-common/src/main/java/io/tinyrpc/provider/common/handler/RpcProviderHandler.java
+++ tinyrpc-provider/tinyrpc-provider-common/src/main/java/io/tinyrpc/provider/common/handler/RpcProviderHandler.java
@@ -1,4 +1,21 @@
 package io.tinyrpc.provider.common.handler;
 
-public class RpcProviderHandler {
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.tinyrpc.protocol.RpcProtocol;
+import io.tinyrpc.protocol.request.RpcRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * RPC服务提供者的 Handler处理类
+ */
+public class RpcProviderHandler extends SimpleChannelInboundHandler<RpcProtocol<RpcRequest>> {
+
+	private final Logger logger = LoggerFactory.getLogger(RpcProviderHandler.class);
+
+	@Override
+	protected void channelRead0(ChannelHandlerContext ctx, RpcProtocol<RpcRequest> msg) throws Exception {
+
+	}
 }
