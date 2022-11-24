587189c00c220d0ebfd4a598f298a8a47430a946
andaicheng <andaicheng@gmail.com>
2022-11-24 15:32:52 +0800

    rpc reflect

--- tinyrpc-reflect/tinyrpc-reflect-asm/src/main/java/io/tinyrpc/reflect/asm/AsmReflectInvoker.java
+++ tinyrpc-reflect/tinyrpc-reflect-asm/src/main/java/io/tinyrpc/reflect/asm/AsmReflectInvoker.java
@@ -1,8 +1,13 @@
 package io.tinyrpc.reflect.asm;
 
 import io.tinyrpc.reflect.api.ReflectInvoker;
+import io.tinyrpc.reflect.asm.proxy.ReflectProxy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
 
 public class AsmReflectInvoker implements ReflectInvoker {
 
@@ -12,6 +17,23 @@ public class AsmReflectInvoker implements ReflectInvoker {
 	public Object invokeMethod(Object serviceBean, Class<?> serviceClass, String methodName, Class<?>[] parameterTypes, Object[] parameters) throws Throwable {
 		logger.info("Use asm reflect type invoke method...");
 
-		return null;
+		Constructor<?> constructor = serviceClass.getConstructor();
+		Object[] constructorParam = new Object[]{};
+		Object instance = ReflectProxy.newProxyInstance(AsmReflectInvoker.class.getClassLoader(),
+			getInvocationHandler(serviceBean),
+			serviceClass,
+			constructor,
+			constructorParam);
+		Method method = serviceClass.getMethod(methodName, parameterTypes);
+		method.setAccessible(true);
+		return method.invoke(instance, parameters);
+	}
+
+	private InvocationHandler getInvocationHandler(Object obj) {
+		return (proxy, method, args) -> {
+			logger.info("Use proxy invoke method...");
+			method.setAccessible(true);
+			return method.invoke(obj, args);
+		};
 	}
 }
