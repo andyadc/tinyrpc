14418ba1295aaf1e11ab5647ffb1e3d496d41c9f
andaicheng <andaicheng@gmail.com>
2022-12-05 14:57:02 +0800

    rpc

---
+++ tinyrpc-common/src/main/java/io/tinyrpc/common/utils/IPUtils.java
@@ -0,0 +1,33 @@
+package io.tinyrpc.common.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.InetAddress;
+import java.util.Objects;
+
+public class IPUtils {
+
+	private static final Logger logger = LoggerFactory.getLogger(IPUtils.class);
+
+	public static InetAddress getLocalInetAddress() {
+		try {
+			return InetAddress.getLocalHost();
+		} catch (Exception e) {
+			logger.error("Get local IP address error. ", e);
+		}
+		return null;
+	}
+
+	public static String getLocalAddress() {
+		return Objects.requireNonNull(getLocalInetAddress()).toString();
+	}
+
+	public static String getLocalHostName() {
+		return Objects.requireNonNull(getLocalInetAddress()).getHostName();
+	}
+
+	public static String getLocalHostIP() {
+		return Objects.requireNonNull(getLocalInetAddress()).getHostAddress();
+	}
+}
