f25d801e60271d2070e9db4ce6b66da409688259
andaicheng <andaicheng@gmail.com>
2022-12-09 09:36:05 +0800

    rpc serialization

--- tinyrpc-serialization/tinyrpc-serialization-kryo/src/main/java/io/tinyrpc/serialization/kryo/KryoSerialization.java
+++ tinyrpc-serialization/tinyrpc-serialization-kryo/src/main/java/io/tinyrpc/serialization/kryo/KryoSerialization.java
@@ -1,8 +1,17 @@
 package io.tinyrpc.serialization.kryo;
 
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import com.esotericsoftware.kryo.serializers.JavaSerializer;
+import io.tinyrpc.common.exception.SerializerException;
 import io.tinyrpc.serialization.api.Serialization;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class KryoSerialization implements Serialization {
 
@@ -10,11 +19,41 @@ public class KryoSerialization implements Serialization {
 
 	@Override
 	public <T> byte[] serialize(T obj) {
-		return new byte[0];
+		logger.info("execute kryo serialize...");
+		if (obj == null) {
+			throw new SerializerException("Serialize object is null");
+		}
+
+		Kryo kryo = new Kryo();
+		kryo.setReferences(false);
+		kryo.register(obj.getClass(), new JavaSerializer());
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		Output output = new Output(baos);
+		kryo.writeClassAndObject(output, obj);
+		output.flush();
+		output.close();
+		byte[] bytes = baos.toByteArray();
+		try {
+			baos.flush();
+			baos.close();
+		} catch (IOException e) {
+			throw new SerializerException(e.getMessage(), e);
+		}
+		return bytes;
 	}
 
 	@Override
 	public <T> T deserialize(byte[] data, Class<T> clazz) {
-		return null;
+		logger.info("execute kryo deserialize...");
+		if (data == null) {
+			throw new SerializerException("deserialize data is null");
+		}
+
+		Kryo kryo = new Kryo();
+		kryo.setReferences(false);
+		kryo.register(clazz, new JavaSerializer());
+		ByteArrayInputStream bais = new ByteArrayInputStream(data);
+		Input input = new Input(bais);
+		return (T) kryo.readClassAndObject(input);
 	}
 }
