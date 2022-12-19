c86d4559c1c6d0bc2b81a4aacb3ee65843effd5f
andaicheng <andaicheng@gmail.com>
2022-12-19 18:18:57 +0800

    rpc

--- tinyrpc-loadbalancer/tinyrpc-loadbalancer-random/src/main/java/io/tinyrpc/loadbalancer/random/RandomServiceLoadBalancer.java
+++ tinyrpc-loadbalancer/tinyrpc-loadbalancer-random/src/main/java/io/tinyrpc/loadbalancer/random/RandomServiceLoadBalancer.java
@@ -1,13 +1,22 @@
 package io.tinyrpc.loadbalancer.random;
 
 import io.tinyrpc.loadbalancer.api.ServiceLoadBalancer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
+/**
+ * 基于随机算法的负载均衡策略
+ *
+ * @param <T>
+ */
 public class RandomServiceLoadBalancer<T> implements ServiceLoadBalancer<T> {
 
+	private static final Logger logger = LoggerFactory.getLogger(RandomServiceLoadBalancer.class);
+
 	@Override
-	public T select(List<T> servers, int hashCode) {
+	public T select(List<T> servers, int hashCode, String sourceIp) {
 		return null;
 	}
 }
