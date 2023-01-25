0bc216b6dcc2aadf3c996b26eda919f155b7c7e0
andaicheng <andaicheng@gmail.com>
2023-01-25 21:36:21 +0800

    codec

--- tinyrpc-codec/src/main/java/io/tinyrpc/codec/RpcDecoder.java
+++ tinyrpc-codec/src/main/java/io/tinyrpc/codec/RpcDecoder.java
@@ -3,13 +3,88 @@ package io.tinyrpc.codec;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ByteToMessageDecoder;
+import io.netty.util.CharsetUtil;
+import io.tinyrpc.common.constants.RpcConstants;
+import io.tinyrpc.common.utils.SerializationUtils;
+import io.tinyrpc.protocol.RpcProtocol;
+import io.tinyrpc.protocol.enumeration.RpcType;
+import io.tinyrpc.protocol.header.RpcHeader;
+import io.tinyrpc.protocol.request.RpcRequest;
+import io.tinyrpc.protocol.response.RpcResponse;
+import io.tinyrpc.serialization.api.Serialization;
 
 import java.util.List;
 
-public class RpcDecoder extends ByteToMessageDecoder {
+/**
+ * 实现RPC解码操作
+ */
+public class RpcDecoder extends ByteToMessageDecoder implements RpcCodec {
 
 	@Override
 	protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf, List<Object> list) throws Exception {
+		if (byteBuf.readableBytes() < RpcConstants.HEADER_TOTAL_LEN) {
+			return;
+		}
+		byteBuf.markReaderIndex();
+		short magic = byteBuf.readShort();
+		if (magic != RpcConstants.MAGIC) {
+			throw new IllegalArgumentException("Magic number is illegal: " + magic);
+		}
 
+		byte msgType = byteBuf.readByte();
+		byte status = byteBuf.readByte();
+		long requestId = byteBuf.readLong();
+
+		ByteBuf serializationTypeByteBuf = byteBuf.readBytes(SerializationUtils.MAX_SERIALIZATION_TYPE_COUNR);
+		String serializationType = SerializationUtils.subString(serializationTypeByteBuf.toString(CharsetUtil.UTF_8));
+
+		int dataLength = byteBuf.readInt();
+		if (byteBuf.readableBytes() < dataLength) {
+			byteBuf.resetReaderIndex();
+			return;
+		}
+
+		byte[] data = new byte[dataLength];
+		byteBuf.readBytes(data);
+
+		RpcType msgTypeEnum = RpcType.findByType(msgType);
+		if (msgTypeEnum == null) {
+			return;
+		}
+
+		RpcHeader header = new RpcHeader();
+		header.setMagic(magic);
+		header.setStatus(status);
+		header.setRequestId(requestId);
+		header.setMsgType(msgType);
+		header.setSerializationType(serializationType);
+		header.setMsgLen(dataLength);
+		// TODO Serialization是扩展点
+		Serialization serialization = getJdkSerialization();
+
+		switch (msgTypeEnum) {
+			case REQUEST:
+				RpcRequest request = serialization.deserialize(data, RpcRequest.class);
+				if (request != null) {
+					RpcProtocol<RpcRequest> protocol = new RpcProtocol<>();
+					protocol.setHeader(header);
+					protocol.setBody(request);
+					list.add(protocol);
+				}
+				break;
+			case RESPONSE:
+				RpcResponse response = serialization.deserialize(data, RpcResponse.class);
+				if (response != null) {
+					RpcProtocol<RpcResponse> protocol = new RpcProtocol<>();
+					protocol.setHeader(header);
+					protocol.setBody(response);
+					list.add(protocol);
+				}
+				break;
+			case HEARTBEAT:
+				//TODO
+				break;
+
+		}
 	}
 }
--- tinyrpc-codec/src/main/java/io/tinyrpc/codec/RpcEncoder.java
+++ tinyrpc-codec/src/main/java/io/tinyrpc/codec/RpcEncoder.java
@@ -1,4 +1,33 @@
 package io.tinyrpc.codec;
 
-public class RpcEncoder {
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+import io.tinyrpc.common.utils.SerializationUtils;
+import io.tinyrpc.protocol.RpcProtocol;
+import io.tinyrpc.protocol.header.RpcHeader;
+import io.tinyrpc.serialization.api.Serialization;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * 实现RPC编码
+ */
+public class RpcEncoder extends MessageToByteEncoder<RpcProtocol<Object>> implements RpcCodec {
+
+	@Override
+	protected void encode(ChannelHandlerContext ctx, RpcProtocol<Object> msg, ByteBuf byteBuf) throws Exception {
+		RpcHeader header = msg.getHeader();
+		byteBuf.writeShort(header.getMagic());
+		byteBuf.writeByte(header.getMsgType());
+		byteBuf.writeByte(header.getStatus());
+		byteBuf.writeLong(header.getRequestId());
+		String serializationType = header.getSerializationType();
+		//TODO Serialization是扩展点
+		Serialization serialization = getJdkSerialization();
+		byteBuf.writeBytes(SerializationUtils.paddingString(serializationType).getBytes(StandardCharsets.UTF_8));
+		byte[] data = serialization.serialize(msg.getBody());
+		byteBuf.writeInt(data.length);
+		byteBuf.writeBytes(data);
+	}
 }
