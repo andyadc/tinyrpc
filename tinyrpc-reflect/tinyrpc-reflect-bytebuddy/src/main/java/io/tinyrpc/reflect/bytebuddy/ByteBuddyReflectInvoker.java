89776c2fb2cda4d7603204d2b68535116b7359bd
andaicheng <andaicheng@gmail.com>
2023-05-01 19:26:47 +0800

    spi reflect bytebuddy

--- tinyrpc-reflect/tinyrpc-reflect-bytebuddy/src/main/java/io/tinyrpc/reflect/bytebuddy/ByteBuddyReflectInvoker.java
+++ tinyrpc-reflect/tinyrpc-reflect-bytebuddy/src/main/java/io/tinyrpc/reflect/bytebuddy/ByteBuddyReflectInvoker.java
@@ -1,19 +1,26 @@
 package io.tinyrpc.reflect.bytebuddy;
 
 import io.tinyrpc.reflect.api.ReflectInvoker;
+import io.tinyrpc.spi.annotation.SPIClass;
 import net.bytebuddy.ByteBuddy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Method;
 
+/**
+ * ByteBuddy反射机制
+ */
+@SPIClass
 public class ByteBuddyReflectInvoker implements ReflectInvoker {
 
 	private static final Logger logger = LoggerFactory.getLogger(ByteBuddyReflectInvoker.class);
 
 	@Override
 	public Object invokeMethod(Object serviceBean, Class<?> serviceClass, String methodName, Class<?>[] parameterTypes, Object[] parameters) throws Throwable {
-		logger.info("Use bytebuddy reflect type invoke method...");
+		if (logger.isDebugEnabled()) {
+			logger.debug("-- bytebuddy reflect --");
+		}
 
 		Class<?> childClass = new ByteBuddy().subclass(serviceClass)
 			.make()
