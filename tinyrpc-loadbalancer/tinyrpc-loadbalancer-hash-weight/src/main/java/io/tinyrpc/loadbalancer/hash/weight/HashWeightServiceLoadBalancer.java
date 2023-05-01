352c75de0011e89e055f9fd8e4d13b20effe92db
andaicheng <andaicheng@gmail.com>
2023-05-01 21:41:00 +0800

    spi loadbalancer hash weight

---
+++ tinyrpc-loadbalancer/tinyrpc-loadbalancer-hash-weight/src/main/java/io/tinyrpc/loadbalancer/hash/weight/HashWeightServiceLoadBalancer.java
@@ -0,0 +1,33 @@
+package io.tinyrpc.loadbalancer.hash.weight;
+
+import io.tinyrpc.loadbalancer.api.ServiceLoadBalancer;
+import io.tinyrpc.spi.annotation.SPIClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+/**
+ * 基于加权Hash算法负载均衡策略
+ */
+@SPIClass
+public class HashWeightServiceLoadBalancer<T> implements ServiceLoadBalancer<T> {
+
+	private static final Logger logger = LoggerFactory.getLogger(HashWeightServiceLoadBalancer.class);
+
+	@Override
+	public T select(List<T> servers, int hashCode) {
+		logger.info("--- Hash weight LoadBalancer ---");
+		if (servers == null || servers.isEmpty()) {
+			return null;
+		}
+
+		hashCode = Math.abs(hashCode);
+		int count = hashCode % servers.size();
+		if (count <= 0) {
+			count = servers.size();
+		}
+		int index = hashCode % count;
+		return servers.get(index);
+	}
+}
--- tinyrpc-test/tinyrpc-test-provider/src/main/java/io/tinyrpc/test/provider/single/RpcSingleServerTests.java
+++ tinyrpc-test/tinyrpc-test-provider/src/main/java/io/tinyrpc/test/provider/single/RpcSingleServerTests.java
@@ -11,7 +11,7 @@ public class RpcSingleServerTests {
 			"127.0.0.1:27880",
 			"127.0.0.1:2181",
 			"zookeeper",
-			"hash",
+			"hashweight",
 			"io.tinyrpc.test",
 			"asm");
 
