2941811933503292bef2af36536b7d861156cda7
andaicheng <andaicheng@gmail.com>
2023-05-01 19:19:36 +0800

    spi reflect javassist

--- tinyrpc-reflect/tinyrpc-reflect-javassist/src/main/java/io/tinyrpc/reflect/javassist/JavassistReflectInvoker.java
+++ tinyrpc-reflect/tinyrpc-reflect-javassist/src/main/java/io/tinyrpc/reflect/javassist/JavassistReflectInvoker.java
@@ -1,19 +1,26 @@
 package io.tinyrpc.reflect.javassist;
 
 import io.tinyrpc.reflect.api.ReflectInvoker;
+import io.tinyrpc.spi.annotation.SPIClass;
 import javassist.util.proxy.ProxyFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Method;
 
+/**
+ * Javassist方式调用方法
+ */
+@SPIClass
 public class JavassistReflectInvoker implements ReflectInvoker {
 
 	private static final Logger logger = LoggerFactory.getLogger(JavassistReflectInvoker.class);
 
 	@Override
 	public Object invokeMethod(Object serviceBean, Class<?> serviceClass, String methodName, Class<?>[] parameterTypes, Object[] parameters) throws Throwable {
-		logger.info("use javassist reflect type invoke method...");
+		if (logger.isDebugEnabled()) {
+			logger.debug("-- javassist reflect --");
+		}
 
 		ProxyFactory proxyFactory = new ProxyFactory();
 		proxyFactory.setSuperclass(serviceClass);
