70f75ce4d137b25b9a879ef673fdfe0fca3b55cc
andaicheng <andaicheng@gmail.com>
2023-05-01 15:39:56 +0800

    spi serialization protostuff

--- tinyrpc-serialization/tinyrpc-serialization-protostuff/src/main/java/io/tinyrpc/serialization/protostuff/ProtostuffSerialization.java
+++ tinyrpc-serialization/tinyrpc-serialization-protostuff/src/main/java/io/tinyrpc/serialization/protostuff/ProtostuffSerialization.java
@@ -1,20 +1,75 @@
 package io.tinyrpc.serialization.protostuff;
 
+import com.dyuproject.protostuff.LinkedBuffer;
+import com.dyuproject.protostuff.ProtostuffIOUtil;
+import com.dyuproject.protostuff.Schema;
+import com.dyuproject.protostuff.runtime.RuntimeSchema;
+import io.tinyrpc.common.exception.SerializerException;
 import io.tinyrpc.serialization.api.Serialization;
+import io.tinyrpc.spi.annotation.SPIClass;
+import org.objenesis.Objenesis;
+import org.objenesis.ObjenesisStd;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Protostuff Serialization
+ */
+@SPIClass
 public class ProtostuffSerialization implements Serialization {
 
 	private static final Logger logger = LoggerFactory.getLogger(ProtostuffSerialization.class);
 
+	private final Map<Class<?>, Schema<?>> cachedSchema = new ConcurrentHashMap<>();
+
+	private final Objenesis objenesis = new ObjenesisStd(true);
+
+	@SuppressWarnings({"unchecked"})
+	private <T> Schema<T> getSchema(Class<T> cls) {
+		Schema<T> schema = (Schema<T>) cachedSchema.get(cls);
+		if (schema == null) {
+			schema = RuntimeSchema.createFrom(cls);
+			cachedSchema.put(cls, schema);
+		}
+		return schema;
+	}
+
+	@SuppressWarnings({"unchecked"})
 	@Override
 	public <T> byte[] serialize(T obj) {
-		return new byte[0];
+		if (logger.isDebugEnabled()) {
+			logger.debug("--- protostuff serialize ---");
+		}
+		Class<T> cls = (Class<T>) obj.getClass();
+		LinkedBuffer buffer = LinkedBuffer.allocate(LinkedBuffer.DEFAULT_BUFFER_SIZE);
+		try {
+			Schema<T> schema = getSchema(cls);
+			return ProtostuffIOUtil.toByteArray(obj, schema, buffer);
+		} catch (Exception e) {
+			throw new SerializerException(e.getMessage(), e);
+		} finally {
+			buffer.clear();
+		}
 	}
 
 	@Override
 	public <T> T deserialize(byte[] data, Class<T> clazz) {
-		return null;
+		if (logger.isDebugEnabled()) {
+			logger.debug("--- protostuff deserialize ---");
+		}
+		if (data == null) {
+			throw new SerializerException("deserialize data is null");
+		}
+		try {
+			T message = (T) objenesis.newInstance(clazz);
+			Schema<T> schema = getSchema(clazz);
+			ProtostuffIOUtil.mergeFrom(data, message, schema);
+			return message;
+		} catch (Exception e) {
+			throw new SerializerException(e.getMessage(), e);
+		}
 	}
 }
