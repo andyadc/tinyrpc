a9d5260c35db6db1b23444e616ebc9db83aeb09b
andaicheng <andaicheng@gmail.com>
2023-05-01 19:12:16 +0800

    spi reflect cglib

--- tinyrpc-reflect/tinyrpc-reflect-cglib/src/main/java/io/tinyrpc/reflect/cglib/CglibReflectInvoker.java
+++ tinyrpc-reflect/tinyrpc-reflect-cglib/src/main/java/io/tinyrpc/reflect/cglib/CglibReflectInvoker.java
@@ -1,18 +1,25 @@
 package io.tinyrpc.reflect.cglib;
 
 import io.tinyrpc.reflect.api.ReflectInvoker;
+import io.tinyrpc.spi.annotation.SPIClass;
 import net.sf.cglib.reflect.FastClass;
 import net.sf.cglib.reflect.FastMethod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Cglib 反射调用方法的类
+ */
+@SPIClass
 public class CglibReflectInvoker implements ReflectInvoker {
 
 	private static final Logger logger = LoggerFactory.getLogger(CglibReflectInvoker.class);
 
 	@Override
 	public Object invokeMethod(Object serviceBean, Class<?> serviceClass, String methodName, Class<?>[] parameterTypes, Object[] parameters) throws Throwable {
-		logger.info("Use cglib reflect type invoke method...");
+		if (logger.isDebugEnabled()) {
+			logger.debug("-- cglib reflect --");
+		}
 
 		FastClass serviceFastClass = FastClass.create(serviceClass);
 		FastMethod fastMethod = serviceFastClass.getMethod(methodName, parameterTypes);
