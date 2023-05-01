b5a3246f9a6c710bbee9ea22ac520b9100b94e39
andaicheng <andaicheng@gmail.com>
2023-05-01 14:40:44 +0800

    spi serialization fst

--- tinyrpc-serialization/tinyrpc-serialization-fst/src/main/java/io/tinyrpc/serialization/fst/FstSerialization.java
+++ tinyrpc-serialization/tinyrpc-serialization-fst/src/main/java/io/tinyrpc/serialization/fst/FstSerialization.java
@@ -2,16 +2,24 @@ package io.tinyrpc.serialization.fst;
 
 import io.tinyrpc.common.exception.SerializerException;
 import io.tinyrpc.serialization.api.Serialization;
+import io.tinyrpc.spi.annotation.SPIClass;
 import org.nustaq.serialization.FSTConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Fst Serialization
+ */
+@SPIClass
 public class FstSerialization implements Serialization {
 
 	private static final Logger logger = LoggerFactory.getLogger(FstSerialization.class);
 
 	@Override
 	public <T> byte[] serialize(T obj) {
+		if (logger.isDebugEnabled()) {
+			logger.debug("--- fst serialize ---");
+		}
 		if (obj == null) {
 			throw new SerializerException("FST serialize object is null");
 		}
@@ -21,6 +29,9 @@ public class FstSerialization implements Serialization {
 
 	@Override
 	public <T> T deserialize(byte[] data, Class<T> clazz) {
+		if (logger.isDebugEnabled()) {
+			logger.debug("--- fst deserialize ---");
+		}
 		if (data == null) {
 			throw new SerializerException("FST deserialize data is null");
 		}
